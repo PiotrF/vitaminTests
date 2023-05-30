2654f71538cd2a9809f845b899dd3894217e9b2f
Piotr Franczak <111983916+PiotrF@users.noreply.github.com>
2023-05-30 20:07:12 +0200

    Faker added and test cases

--- tests_all/test_log_in_manually.py
+++ tests_all/test_log_in_manually.py
@@ -1,4 +1,9 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+from faker import Faker
+
+fake = Faker()
+
+random_email = fake.email()
 
 
 def test_log_in_manually(playwright: Playwright) -> None:
@@ -22,5 +27,39 @@ def test_log_in_manually(playwright: Playwright) -> None:
     context.close()
     browser.close()
 
+def test_log_in_incorrect_data(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://develop.nutrable.com/")
+
+    page.get_by_text('Accept All').click()
+    page.get_by_label('login').click()
+    page.get_by_text('Continue with Email').click()
+    page.locator("#email").fill('piotr.franczak@cledar.pl')
+    page.locator("#password").fill('IncorrectPassword')
+    page.get_by_text('Login').click()
+
+    # expect(page.query_selector('.text-danger .py-2')).to_contain_text('Invalid username or password.')
+    expect(page.get_by_text("Invalid username or password.")).to_have_text('Invalid username or password.')
 
 
+def test_creating_new_user(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://develop.nutrable.com/")
+
+    page.get_by_text('Accept All').click()
+    page.get_by_label('login').click()
+    page.get_by_role("link", name="Create Account").click()
+    page.get_by_placeholder("First Name").fill("name")
+    page.get_by_placeholder("Last Name").fill("last name")
+    page.get_by_placeholder("Email Address").fill(random_email)
+    page.get_by_placeholder("Password", exact=True).fill("Tester1234!")
+    page.get_by_placeholder("Confirm Password").fill("Tester1234!")
+    page.locator("#kc-register-form path").click()
+    page.get_by_role("button", name="Create Account").click()
+
+    expect(page.get_by_role("heading", name="Confirm Your Email")).to_have_text('Confirm Your Email')
+    expect(page.get_by_text("An email has been sent to you to complete your registration. Please verify your ")).to_contain_text('An email has been sent to you to complete your registration. Please verify your email address to continue.')
